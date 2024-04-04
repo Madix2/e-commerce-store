20681da1861f44980e8c46d92fb11c4cd8d549f7
Madix2 <madubanyamadimetja@gmail.com>
2024-04-05 00:35:32 +0200

    1Commit5April-Looking good so far

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import ProductCard from "./components/productcard";
+import ProductCard from "./components/products/productcard";
 import { products } from "@/utils/products";
 import { Truncer } from "@/utils/truncer";
 import Container from "./components/container";
@@ -13,10 +13,10 @@ export default function Home() {
         <div>
           <HomeBanner />
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {
             products.map((product: any) =>{
-              return <div>{Truncer(product.name)}</div>;
+              return <ProductCard data={product} />
             })
           }
         </div>
