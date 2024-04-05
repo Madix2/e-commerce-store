f969b8445503bc41d17706d8751813e6f07743c3
Madix2 <madubanyamadimetja@gmail.com>
2024-04-05 18:55:25 +0200

    Its that time

--- app/components/products/productcard.tsx
+++ app/components/products/productcard.tsx
@@ -12,7 +12,7 @@ interface ProductCardProps{
 const ProductCard: React.FC<ProductCardProps> = ({data}) =>{
     return (
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm ">
-            <div className="flex flex=col items-center w-full gap-1">
+            <div className="flex flex-col items-center w-full gap-1">
                 <div className="aspect-square overflow-hidden relative w-full">
                     <Image fill src={data.images[0].image} alt={data.name} className="w-full h-full object-contain"/>
                 </div>
@@ -20,10 +20,12 @@ const ProductCard: React.FC<ProductCardProps> = ({data}) =>{
                     {Truncer(data.name)}
                 </div>
                 <div>
-                    {/* <Rating value={data.rating} readOnly />  */}
+                    <Rating value={data.rating} readOnly /> 
                 </div>
-                <div> {data.reviews.length} reviews</div>
-                <div> {FPrice(data.price)} </div>
+                <div className=""> 
+                    {data.reviews.length} reviews
+                </div>
+                <div className=""> {FPrice(data.price)} </div>
             </div>
         </div> 
     )
